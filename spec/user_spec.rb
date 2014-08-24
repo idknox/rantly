d951ebff44f858058114e2d5149f767ef9faee68
Ian Knox <knoxid@gmail.com>
2014-08-24 17:03:57 -0600

    user can log out

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -67,4 +67,27 @@ feature "User Registration" do
     expect(page).to have_content "Ian Knox"
   end
 
+  scenario "Logged in user can logout" do
+    visit "/"
+    click_on "Join"
+
+    fill_in "Username", :with => "idknox"
+    fill_in "Password", :with => "123"
+    fill_in "First name", :with => "Ian"
+    fill_in "Last name", :with => "Knox"
+    fill_in "Bio", :with => "Student"
+    within ".frequency-container" do
+      choose "Daily"
+    end
+    click_on "Register"
+
+    click_on "Login"
+    fill_in "Username", :with => "idknox"
+    fill_in "Password", :with => "123"
+    click_on "LOGIN"
+
+    click_on "LOGOUT"
+
+    expect(page).to have_content "Rantly", "Let it all out"
+  end
 end
