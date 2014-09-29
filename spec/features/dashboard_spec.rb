de960b7cf7be4fc0da79986fcebb5d080d81bedd
Ian Knox <knoxid@gmail.com>
2014-09-29 13:26:23 -0600

    added rant length val

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -10,12 +10,19 @@ feature "Rants" do
   end
 
   scenario "User can create and delete a new Rant" do
-    expect(page).to have_content("Ian Knox", "My Rants Ian blah blah blah")
+    test_rant = "Rantly has allowed a certain freedom I never thought possible.
+      Now I can rant to the internet at large, without having to worry
+      about my thoughts. I just type it out, hit click, and feel
+      the surge of relief. Thanks Rantly, you've truly made my most
+      inane rants heard. I remember a time when I was able to truly express
+      myself. It's really frustrating when you want to be heard and no one..."
+
+    expect(page).to have_content("Ian Knox", "My Rants Ian #{test_rant}")
 
     click_on "Delete"
 
     expect(page).to have_content("Ian Knox", "My Rants")
-    expect(page).to_not have_content("blah blah blah")
+    expect(page).to_not have_content(test_rant)
   end
 
   scenario "User can see others' rants" do
@@ -27,8 +34,17 @@ feature "Rants" do
       bio: 'guy',
       rant_frequency: 'Monthly'
     })
-    create_rant(other_user.id, subject: 'Other Stuff', body: 'more blah')
+
+    test_rant = "Rantly has allowed a certain freedom I never thought possible.
+      Now I can rant to the internet at large, without having to worry
+      about my thoughts. I just type it out, hit click, and feel
+      the surge of relief. Thanks Rantly, you've truly made my most
+      inane rants heard. I remember a time when I was able to truly express
+      myself. It's really frustrating when you want to be heard and no one..."
+
+    create_rant(other_user.id, subject: 'Other Stuff', body: test_rant)
+
     visit "/"
-    expect(page).to have_content("Latest Rants Bob Follow more blah")
+    expect(page).to have_content("Latest Rants Bob Follow #{test_rant}")
   end
 end
--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -10,9 +10,16 @@ def create_user(overrides = {})
 end
 
 def create_rant(id, overrides = {})
+  test_rant = "Rantly has allowed a certain freedom I never thought possible.
+      Now I can rant to the internet at large, without having to worry
+      about my thoughts. I just type it out, hit click, and feel
+      the surge of relief. Thanks Rantly, you've truly made my most
+      inane rants heard. I remember a time when I was able to truly express
+      myself. It's really frustrating when you want to be heard and no one..."
+
   Rant.create!({
                  subject: 'Stuff',
-                 body: 'blah blah blah',
+                 body: test_rant,
                  user_id: id
                }.merge(overrides))
 end
